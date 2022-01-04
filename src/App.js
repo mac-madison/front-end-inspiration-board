8c61e64bdbff88d19c93ea71076468b77fdaf41d
Citlalli I Zarate <37793116+MiffyBruna@users.noreply.github.com>
2022-01-04 12:23:56 -0800

    Added  HTML and css for App.js and App.css

--- src/App.js
+++ src/App.js
@@ -8,38 +8,48 @@ function App() {
   ///BOARDS- DINAMIC TEXT LIST( LIST OF BOARDS---EACH BOARD TITLE IS BUTTON)
   //SELECTED BOARD- WHEN BOARD TITLE CLICKED IN BOARDS---- DISPLAYS THE TITLE OF BOARD
 
+
   ///DISPLAY CARDS--- SHOWS NAME OF SELECTED BOARD--- SHOWS CARDS( LIKED(DIPLAYS LIKES), DELETED)
 
 
 return (
-  <div className="App">
-    <header className="app-header"> <h1> Inspiration Board</h1> </header>
-    <body>
-      <section className="boards-header-container">
-        <h2>Boards</h2>
-        <div id="scroll-box-div">
-          <ol id="all-board-titles"></ol>
-        </div>
-      </section>
+  <body>
+    <div className="App">
+      <div class="page-container">
+        <div class="content-container">
+          <h1>Inspiration Board</h1>
+          
+          <section class="boards-container">
+          <section>
+            <h2>Boards</h2>
+            <ol class="boards-list">
+              <li><div>Pick-me-up Quotes</div>
+              </li>
+            </ol>
+          </section>
 
-      <section className= "boards-container">
-        <h3>Selected Board</h3>
-      </section>
-      
-      <section className="create-new-board-container">
+          <section id="selected-boards-section">
+            <h3>Selected Board</h3><p>Select a Board from the Board List!</p>
+          </section>
+
+      <section class="new-board-form-container">
         <h4>Create a New Board</h4>
-      </section>
-      
-      <section className="create-new-card-container">
-        <h5>Create a New Board</h5>
-      </section>
-
-      <footer>
-        <span> This is a Demo, please be gentle!</span>
-      </footer>
-    
+        <form class="new-board-form">
+          <label>Title</label>
+          <input type="text" class="invalid-form-input" value=""/>
+            <label>Owner's Name</label>
+            <input type="text" class="invalid-form-input" value=""/><p>Preview:  - </p>
+            <input type="Submit" disabled="" class="new-board-form-submit-btn"/>
+        </form>
+        <span class="new-board-form-toggle-btn">Hide New Board Form</span>
+        </section>
+        
+        </section></div>
+        <footer>
+          <span>This is a demo! Please be gentle!</span></footer>
+          </div></div>
     </body>
-  </div>
+  
 );
 }
 
