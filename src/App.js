116dd06506742f7c0dcdd4f1c766fa3a812c166d
Ana Gabriele <gabbanasoares@gmail.com>
2022-01-04 20:25:50 -0800

    Implement CreateCard component  and create functionality to addCard and getCards

--- src/App.js
+++ src/App.js
@@ -1,56 +1,115 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CreateCard from "./components/CreateCard";
+import "./App.css";
+
+const URL = "https://kinder-code.herokuapp.com";
 
 function App() {
-  
   /// CREATE NEW BOARD- (TEXT INPUTS----TITLE, OWNER NAME) SUBMIT BUTTON
   //CREATE NEW CARDS - CREATE NEW CARD- (TEXT INPUTS----MESSAGE) SUBMIT BUTTON
 
   ///BOARDS- DINAMIC TEXT LIST( LIST OF BOARDS---EACH BOARD TITLE IS BUTTON)
   //SELECTED BOARD- WHEN BOARD TITLE CLICKED IN BOARDS---- DISPLAYS THE TITLE OF BOARD
 
-
   ///DISPLAY CARDS--- SHOWS NAME OF SELECTED BOARD--- SHOWS CARDS( LIKED(DIPLAYS LIKES), DELETED)
 
+  const [cards, setCards] = useState([]);
 
-return (
-  <body>
-    <div className="App">
-      <div class="page-container">
-        <div class="content-container">
-          <h1>Inspiration Board</h1>
-          
-          <section class="boards-container">
-          <section>
-            <h2>Boards</h2>
-            <ol class="boards-list">
-              <li><div>Pick-me-up Quotes</div>
-              </li>
-            </ol>
-          </section>
+  const [status, setStatus] = useState("Loading...");
 
-          <section id="selected-boards-section">
-            <h3>Selected Board</h3><p>Select a Board from the Board List!</p>
-          </section>
+  useEffect(() => {
+    axios
+      .get(`${URL}/cards`)
+      .then((res) => {
+        const newCards = res.data.map((card) => {
+          return {
+            card_id: card.card_id,
+            message: card.message,
+            likes_count: card.likes_count,
+            board_id: card.board_id,
+          };
+        });
+        setCards(newCards);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-      <section class="new-board-form-container">
-        <h4>Create a New Board</h4>
-        <form class="new-board-form">
-          <label>Title</label>
-          <input type="text" class="invalid-form-input" value=""/>
-            <label>Owner's Name</label>
-            <input type="text" class="invalid-form-input" value=""/><p>Preview:  - </p>
-            <input type="Submit" disabled="" class="new-board-form-submit-btn"/>
-        </form>
-        <span class="new-board-form-toggle-btn">Hide New Board Form</span>
-        </section>
-        
-        </section></div>
-        <footer>
-          <span>This is a demo! Please be gentle!</span></footer>
-          </div></div>
+  const addCard = ({ message }) => {
+    const board_id = 1;
+    axios
+      .post(`${URL}/boards/${board_id}/cards`, {
+        message,
+        likes_count: 0,
+        board_id,
+      })
+      .then((res) => {
+        const newCard = {
+          id: res.data.card_id,
+          likes_count: res.data.likes_count,
+          message: res.data.message,
+          board_id: res.data.board_id,
+        };
+        setCards([...cards, newCard]);
+      })
+      .catch((err) => console.log(err.response.data));
+  };
+
+  return (
+    <body>
+      <div className="App">
+        <div class="page-container">
+          <div class="content-container">
+            <h1>Inspiration Board</h1>
+
+            <section class="boards-container">
+              <section>
+                <h2>Boards</h2>
+                <ol class="boards-list">
+                  <li>
+                    <div>Pick-me-up Quotes</div>
+                  </li>
+                </ol>
+              </section>
+
+              <section id="selected-boards-section">
+                <h3>Selected Board</h3>
+                <p>Select a Board from the Board List!</p>
+              </section>
+
+              <section class="new-board-form-container">
+                <h4>Create a New Board</h4>
+                <form class="new-board-form">
+                  <label>Title</label>
+                  <input type="text" class="invalid-form-input" value="" />
+                  <label>Owner's Name</label>
+                  <input type="text" class="invalid-form-input" value="" />
+                  <p>Preview: - </p>
+                  <input
+                    type="Submit"
+                    disabled=""
+                    class="new-board-form-submit-btn"
+                  />
+                </form>
+                <span class="new-board-form-toggle-btn">
+                  Hide New Board Form
+                </span>
+              </section>
+
+              <section>
+                <CreateCard addCardCallback={addCard} />
+              </section>
+            </section>
+          </div>
+          <footer>
+            <span>This is a demo! Please be gentle!</span>
+          </footer>
+        </div>
+      </div>
     </body>
-  
-);
+  );
 }
 
 export default App;
--- src/components/CreateCard.js
+++ src/components/CreateCard.js
@@ -1,8 +1,66 @@
-const CreateCard = () => {
+import { useState } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import "./CreateCard.css";
 
-    // should sumbit the user input fields
-    // Card Message 
-    //
-}
+const URL = "https://kinder-code.herokuapp.com";
+
+const CreateCard = ({ addCardCallback, board_id }) => {
+  // get id from clickEvent on board , pass this value as board_id
+
+  board_id = 1;
+  const [newCardData, setNewCardData] = useState({
+    board_id: "",
+    message: "",
+  });
+
+  const inputValid = () => {
+    return newCardData.message.length <= 40;
+  };
+
+  const onMessageChange = (event) => {
+    setNewCardData({
+      ...newCardData,
+      message: event.target.value,
+    });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (!inputValid()) {
+      return;
+    }
+    // console.log("me");
+    // create a new card
+    addCardCallback(newCardData);
+    // reset state
+    setNewCardData({
+      board_id: "",
+      message: "",
+    });
+  };
+
+  return (
+    <section>
+      <form onSubmit={onSubmit}>
+        <h2>Create a New Card</h2>
+
+        <label htmlFor="message">Message</label>
+        <input
+          type="text"
+          value={newCardData.message}
+          onChange={onMessageChange}
+        />
+        <p> Preview: {newCardData.message} </p>
+        <button type="submit">submit</button>
+      </form>
+    </section>
+  );
+};
+
+CreateCard.propTypes = {
+  board_id: PropTypes.number.isRequired,
+  addCardCallback: PropTypes.func.isRequired,
+};
 
 export default CreateCard;
